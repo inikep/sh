b86e336fefea51060ea9955fc150158f87a48938
Luis Donoso <luis.donoso@percona.com>
2021-07-06 13:54:02 +0200

    Use the same filter to decide if a commit is skipped as gen_merge_plan.sh

--- fb_prod_skip_commits.sh
+++ fb_prod_skip_commits.sh
@@ -15,7 +15,7 @@ for COMMIT in $(git rev-list --reverse $FBPROD2 ^$FBPROD1)
 do
   ROCKS_FILES=`git diff --name-only $COMMIT~..$COMMIT | grep rocksdb`
   SQL_FILES=`git diff --name-only $COMMIT~..$COMMIT | grep "sql/"`
-  if [[ "$ROCKS_FILES" == "" ]] || [[ "$SQL_FILES" != "" ]]; then
+  if [[ "$ROCKS_FILES" == "" ]]; then
     fb_skip_commit.sh $FBPROD2 $PSNUM $COMMIT
   fi
 done

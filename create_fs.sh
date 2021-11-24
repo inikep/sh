c1831dd382738c4ae48faed446b36d648229a1e1
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2021-11-24 13:49:12 +0100

    add "create_fs.sh"

---
+++ create_fs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEVICE=$1
+MOUNT_POINT=$2
+FILE_SYSTEM=$3
+
+if [ $# -lt 3 ]; then
+  echo "usage: $0 [DEVICE] [MOUNT_POINT] [FILE_SYSTEM]"
+  exit
+fi     
+
+df -T | grep $DEVICE
+umount $MOUNT_POINT
+if [ "$FILE_SYSTEM" == "xfs" ]; then
+    mkfs.$FILE_SYSTEM -f $DEVICE
+else
+    mkfs.$FILE_SYSTEM -F $DEVICE
+fi
+mkdir -p $MOUNT_POINT
+mount $DEVICE $MOUNT_POINT
+chmod a+rwx $MOUNT_POINT
+df -T | grep $DEVICE

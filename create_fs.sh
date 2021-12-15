67a2db0b7d0edfbd5e6d02ce2d4150e0fcfc0b38
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2021-12-15 13:56:21 +0000

    add $MKFS_OPTIONS to create_fs.sh

--- create_fs.sh
+++ create_fs.sh
@@ -3,18 +3,19 @@
 DEVICE=$1
 MOUNT_POINT=$2
 FILE_SYSTEM=$3
+MKFS_OPTIONS=$4
 
 if [ $# -lt 3 ]; then
-  echo "usage: $0 [DEVICE] [MOUNT_POINT] [FILE_SYSTEM]"
+  echo "usage: $0 [DEVICE] [MOUNT_POINT] [FILE_SYSTEM] <MKFS_OPTIONS>"
   exit
-fi     
+fi
 
 df -T | grep $DEVICE
 umount $MOUNT_POINT
 if [ "$FILE_SYSTEM" == "xfs" ]; then
-    mkfs.$FILE_SYSTEM -f -K $DEVICE
+    mkfs.$FILE_SYSTEM -f $MKFS_OPTIONS $DEVICE
 else
-    mkfs.$FILE_SYSTEM -F -K $DEVICE
+    mkfs.$FILE_SYSTEM -F $MKFS_OPTIONS $DEVICE
 fi
 mkdir -p $MOUNT_POINT
 mount $DEVICE $MOUNT_POINT
